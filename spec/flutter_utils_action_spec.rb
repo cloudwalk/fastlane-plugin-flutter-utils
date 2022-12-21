f5e3f6b1c9361094d81325762ea45b404f34c22a
Jonathan Ferreira <jonathan@cloudwalk.io>
2022-12-21 13:27:29 -0300

    test: create basic tests

--- spec/flutter_utils_action_spec.rb
+++ spec/flutter_utils_action_spec.rb
@@ -1,9 +1,17 @@
-describe Fastlane::Actions::FlutterUtilsAction do
+describe Fastlane::FlutterUtils do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The flutter_utils plugin is working!")
+    it 'prints a message when the build number  action is running' do
+      expect(Fastlane::UI).to receive(:message).with("Settings build number 20 in example target")
+      expect(Fastlane::UI).to receive(:message).with("Successfully updated build number!")
 
-      Fastlane::Actions::FlutterUtilsAction.run(nil)
+      Fastlane::Actions::SetIosBuildNumberAction.run(target_name: 'example', build: '20', xcodeproj: 'spec/fixtures/example.xcodeproj')
+    end
+
+    it 'prints a message when the version number action is running' do
+      expect(Fastlane::UI).to receive(:message).with("Settings version number 1.1.1 in example target")
+      expect(Fastlane::UI).to receive(:message).with("Successfully updated version number!")
+
+      Fastlane::Actions::SetIosVersionNumberAction.run(target_name: 'example', version: '1.1.1', xcodeproj: 'spec/fixtures/example.xcodeproj')
     end
   end
 end
